93a58ecbbe63e05f8581d9ff048ab7fa2a6d05c1
Cole Bemis <colebemis@gmail.com>
2018-09-20 09:54:24 -0700

    docs: Remove gatsby docs

--- docs-gatsby/src/components/container.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Box from './box'
-
-function Container(props) {
-  return <Box maxWidth={1200} width="95%" mx="auto" px={4} {...props} />
-}
-
-export default Container
